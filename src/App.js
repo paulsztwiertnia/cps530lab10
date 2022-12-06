fa91b35b4ab55937ca636243608cad666dbcc0da
paulsztwiertnia <102555250+paulsztwiertnia@users.noreply.github.com>
2022-12-06 16:05:54 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        To build this website, i used react.js because its easy to intergrate witihin vs code and to deploy the website. 
+        The process was quite simple, by running a couple terminal commands i was able to create a directory and start working on my 
+        react.js website.
+        The only difficulties i ran into was launching the application using digitalocean.com, the interface was a little confusing at first
+        but after some trouble shooting i was able to delpoy my website.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
